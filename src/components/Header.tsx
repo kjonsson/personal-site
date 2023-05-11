631bd1194cd62b7e111b223b94670be95a0370da
kjonsson <kristjanj11@gmail.com>
2023-05-11 09:32:41 +0000

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,56 @@
+"use client";
+
+import Link from "next/link";
+
+import { motion } from "framer-motion";
+
 const Header = () => {
   return (
-    <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 ">
-        <div className="flex flex-wrap items-center justify-between">
-          <div
-            className="items-center justify-between w-full"
-            id="mobile-menu-2"
-          >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 "
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
-                >
-                  Movies
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
-                >
-                  Tv
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
-                >
-                  Books
-                </a>
-              </li>
-            </ul>
-          </div>
+    <header className="sticky top-0 p-8 mx-auto max-w-7xl">
+      <motion.div
+        initial={{ x: -500 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-wrap items-center justify-between"
+      >
+        <div className="items-center justify-between w-full" id="mobile-menu-2">
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
+              <Link
+                href=""
+                className="block py-2 pl-3 pr-4 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 "
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/movies"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+              >
+                Movies
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/tv"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+              >
+                Tv
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/books"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+              >
+                Books
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </motion.div>
     </header>
   );
 };
